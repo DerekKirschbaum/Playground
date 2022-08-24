3f9daf3d5ba7539869794eb290ca0792ca215e1d
Kirschbaum <derekk14720@stu.powayusd.com>
2022-08-24 14:12:09 -0700

    Added Numbers

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I will add numbers!!!!");
+        System.out.println("5 + 6 = " + addNums(5, 6));
+        System.out.println("52349857 + 62349875 = " + addNums(52349857, 62349875));
+
+    }
+
+    public static int addNums (int a, int b) {
+        return a + b;
+    }
+}
